41bdc6b4ab4bc536c5b2288b3e4d86046b6feb35
zzzz100344 <ogame033641685@gmail.com>
2025-02-05 01:28:13 +0800

    透過增強的狀態管理重構聊天抽屜元件
    - 用面板取代側邊欄以改進聊天介面
    - 新增未讀訊息計數跟踪
    - 實現動態可見性與折疊狀態
    - 改善聊天互動和使用者體驗

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RadioComponent } from './radio/radio.component';
 import { ButtonModule } from 'primeng/button';
-
+import { ChatDrawerComponent } from './chat-drawer/chat-drawer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RadioComponent, ButtonModule ],
+  imports: [RouterOutlet, RadioComponent, ButtonModule, ChatDrawerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })

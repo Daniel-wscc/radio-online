d516c1df848f063b8f056cff67564f5c30b26b52
zzzz100344 <ogame033641685@gmail.com>
2025-02-06 23:30:00 +0800

    fix(angular): 嘗試修正socket.io會有無法連線的問題

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,3 @@
 import { Routes } from '@angular/router';
-import { RadioComponent } from './radio/radio.component';
-import { YoutubeRadioComponent } from './youtube-radio/youtube-radio.component';
 
 export const routes: Routes = [];

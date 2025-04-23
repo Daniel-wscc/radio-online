34aab8a13b974276d85901b16bca9ff986cf4163
Daniel <wscc1031@gmail.com>
2025-04-23 14:48:16 +0800

    feat(config)：嘗試修正session ID unknown問題 - 新增 Socket.io 配置選項以支援憑證傳遞 - 在 Socket.io 配置中新增 withCredentials 選項以改善跨域請求的處理

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,7 +16,8 @@ const config: SocketIoConfig = {
     reconnection: true,
     reconnectionAttempts: 5,
     reconnectionDelay: 1000,
-    timeout: 5000
+    timeout: 5000,
+    withCredentials: true
   }
 };
 
